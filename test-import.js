2dda6738e4de3ddcaec7959c91bcf1360d8df827
Forgotten Formula PMA <michael@forgottenformula.com>
2026-03-24 04:59:03 -0500

    chore: cleanup and push remaining changes

--- test-import.js
+++
@@ -1,5 +0,0 @@
-try {
-  import('./server/scripts/extract-training-data.ts').then(console.log).catch(err => console.error("IMPORT ERROR:", err));
-} catch(e) {
-  console.error("SYNC ERROR:", e);
-}
